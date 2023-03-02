2a6176eb56710ef3e61837bfb8a7c0451945abc3
b4cktr4ck5r3 <flennertom@gmail.com>
2023-03-02 11:37:25 -0500

    adding logic for calculation

--- AllowanceCalculator.cs
+++ AllowanceCalculator.cs
@@ -1,35 +1,61 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace AllowanceCalculator.Function
 {
     public static class AllowanceCalculator
     {
         [FunctionName("AllowanceCalculator")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            string fixedAllowanceQueryParam = req.Query["fixedAllowance"];
+            string geographicAllowanceQueryParam = req.Query["geographicAllowance"];
+            string housingSupportQueryParam = req.Query["housingSupport"];
 
-            string name = req.Query["name"];
+            if (string.IsNullOrEmpty(fixedAllowanceQueryParam))
+                return new BadRequestObjectResult("Missing the query parameter fixedAllowance in HTTP GET Request");
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            if (string.IsNullOrEmpty(geographicAllowanceQueryParam))
+                return new BadRequestObjectResult("Missing the query parameter geographicAllowance in HTTP GET Request");
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            decimal fixedAllowance, geographicAllowance;
 
-            return new OkObjectResult(responseMessage);
+            if (!decimal.TryParse(fixedAllowanceQueryParam, out fixedAllowance))
+                return new ObjectResult(value: "Error while parsing fixedAllowanceQueryParam to decimal.") { StatusCode = StatusCodes.Status500InternalServerError };
+
+            if (!decimal.TryParse(geographicAllowanceQueryParam, out geographicAllowance))
+                return new ObjectResult(value: "Error while parsing geographicAllowanceQueryParam to decimal.") { StatusCode = StatusCodes.Status500InternalServerError };
+
+            if (string.IsNullOrEmpty(housingSupportQueryParam))
+            {
+                return new OkObjectResult(new
+                {
+                    fixedAllowance = fixedAllowance,
+                    geographicalAllowance = geographicAllowance,
+                    totalAllowance = fixedAllowance + geographicAllowance,
+                });
+            }
+            else
+            {
+                decimal housingSupport;
+
+                if (!decimal.TryParse(housingSupportQueryParam, out housingSupport))
+                    return new ObjectResult(value: "Error while parsing housingSupportQueryParam to decimal.") { StatusCode = StatusCodes.Status500InternalServerError };
+
+                return new OkObjectResult(new
+                {
+                    fixedAllowance = fixedAllowance,
+                    geographicalAllowance = geographicAllowance,
+                    depressionAllowance = geographicAllowance * (decimal)0.2,
+                    housingSupport = housingSupport,
+                    totalAllowance = fixedAllowance + geographicAllowance * (decimal)0.8 + housingSupport,
+                });
+            }
         }
     }
 }
